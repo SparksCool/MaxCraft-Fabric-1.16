a628d13e2744e66fc764682ee338898e46b868e2
SparksCool <70763188+SparksCool@users.noreply.github.com>
2021-05-04 17:50:01 -0400

    removed unneeded imports

--- src/main/java/net/sparks/maxcraft/potions/genericPotion.java
+++ src/main/java/net/sparks/maxcraft/potions/genericPotion.java
@@ -1,19 +1,10 @@
 package net.sparks.maxcraft.potions;
 
-import net.minecraft.advancement.criterion.Criteria;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.potion.Potion;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.UseAction;
-import net.minecraft.world.World;
 
 public class genericPotion extends Item {
     public StatusEffect Effect;
