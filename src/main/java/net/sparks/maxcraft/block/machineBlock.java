d6704d02ce3c9b59ba1bf5fc1b59037977b4f2d5
SparksCool <70763188+SparksCool@users.noreply.github.com>
2021-05-11 11:29:43 -0400

    Gonna try making it a chest again lol

--- src/main/java/net/sparks/maxcraft/block/machineBlock.java
+++ src/main/java/net/sparks/maxcraft/block/machineBlock.java
@@ -2,12 +2,15 @@ package net.sparks.maxcraft.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
 
-public class machineBlock extends Block{
+public class machineBlock extends BlockWithEntity{
 
     public machineBlock(Settings settings) {
         super(settings);
@@ -20,5 +23,10 @@ public class machineBlock extends Block{
     public BlockState getPlacementState(ItemPlacementContext ctx) {
 		return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
 	}
+    @Override
+    public BlockEntity createBlockEntity(BlockView world) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
